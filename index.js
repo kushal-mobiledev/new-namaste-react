304757eb17a089a0ac8f396f419e4ac8668db1db
Kushal Desai <kushaldesai1990@gmail.com>
2025-10-05 10:39:26 +0530

    episode-1

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const HeadingComponent = () => {
+  <div id="container">
+    <h1>Namaste React Functional Component</h1>
+  </div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
